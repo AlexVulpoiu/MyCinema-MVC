fc2313c7fb5635a39d9c9af2c2bc4551b7a06f79
AlexVulpoiu <vulpoiu_alexandru@yahoo.com>
2023-04-08 17:28:31 +0300

    initial commit

---
+++ src/main/java/com/unibuc/fmi/mycinemamvc/MyCinemaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.unibuc.fmi.mycinemamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCinemaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCinemaMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unibuc/fmi/mycinemamvc/MyCinemaMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibuc.fmi.mycinemamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCinemaMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8a4b49907e68394b105d7d7b8c2b48defbde33f8
AlexVulpoiu <vulpoiu_alexandru@yahoo.com>
2023-04-23 23:12:54 +0300

    remove unused import

--- src/main/java/com/unibuc/fmi/mycinemamvc/domain/Actor.java
+++ src/main/java/com/unibuc/fmi/mycinemamvc/domain/Actor.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.unibuc.fmi.mycinemamvc.validators.OnlyLetters;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
